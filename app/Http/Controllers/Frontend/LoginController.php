491bd29937be29b013958529371ae79328601599
mahdyadel00 <mahdyadel00@gmail.com>
2023-02-15 23:15:23 +0200

    test google

--- app/Http/Controllers/Frontend/LoginController.php
+++ app/Http/Controllers/Frontend/LoginController.php
@@ -37,7 +37,7 @@ class LoginController extends Controller
     public function callbackHandel()
     {
         $user = Socialite::driver('google')->user();
-            dd($user->user);
+            dd($user->user->picture);
         $data = User::where('email', $user->email)->first();
         if ($data) {
             Auth::login($data);
