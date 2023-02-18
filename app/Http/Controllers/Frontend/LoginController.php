e686ef5756c5bb52a95817ff4dfa649dcccdc3a1
mahdygeexar <mahdy@geexar.dev>
2023-02-18 14:51:19 +0200

    test instgram

--- app/Http/Controllers/Frontend/LoginController.php
+++ app/Http/Controllers/Frontend/LoginController.php
@@ -84,34 +84,6 @@ class LoginController extends Controller
         }
         return redirect()->back()->with('error', 'Email or password is incorrect');
     }
-    //login with Instgram
-
-    public function instgram()
-    {
-        return Socialite::driver('instgram')->redirect();
-    }
-    public function callbackHandelInstgram()
-    {
-        dd('her');
-        $user = Socialite::driver('instgram')->user();
-        dd($user);
-        $data = User::where('email', $user->email)->first();
-        if ($data) {
-            Auth::login($data);
-            return redirect()->route('home')->with('success', 'Login Successfully BY Instgram');
-        } else {
-            User::updateOrCreate([
-                'instgram_id' => $user->id,
-            ], [
-                'name'     => $user->name,
-                'password' => $user->token,
-                'image'    => $user->attributes['avatar_original'],
-            ]);
-
-            return redirect()->route('login.show')->with('success', 'Registration Successfully BY Facebook');
-        }
-        return redirect()->back()->with('error', 'Email or password is incorrect');
-    }
 
     protected function logout()
     {
