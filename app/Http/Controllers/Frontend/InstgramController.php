c37f3113df629a03caa3b867f522e2cb5211a355
mahdygeexar <mahdy@geexar.dev>
2023-02-19 14:25:46 +0200

    login by instgram

--- app/Http/Controllers/Frontend/InstgramController.php
+++ app/Http/Controllers/Frontend/InstgramController.php
@@ -68,44 +68,9 @@ class InstgramController extends Controller
             ]);
             return redirect()->route('login.show')->with('success', 'Registration Successfully BY Instgram');
         } else {
-            Auth::login($user);
+            Auth::login($data);
             return redirect()->route('home')->with('success', 'Login Successfully BY Instgram');
         }
-
-
-        // $user = new User();
-
-        // // Get access token
-        // $response = $user->request('POST', 'https://api.instagram.com/oauth/access_token', [
-        //     'form_params' => [
-        //         'app_id' => $appId,
-        //         'app_secret' => $secret,
-        //         'grant_type' => 'authorization_code',
-        //         'redirect_uri' => $redirectUri,
-        //         'code' => $code,
-        //     ]
-        // ]);
-
-        // if ($response->getStatusCode() != 200) {
-        //     return redirect()->route('home')->with('error', 'Unauthorized login to Instagram.');
-        // }
-
-        // $content = $response->getBody()->getContents();
-        // $content = json_decode($content);
-
-        // $accessToken = $content->access_token;
-        // $userId = $content->user_id;
-
-        // // Get user info
-        // $response = $user->request('GET', "https://graph.instagram.com/me?fields=id,username,account_type&access_token={$accessToken}");
-
-        // $content = $response->getBody()->getContents();
-        // $oAuth = json_decode($content);
-
-        // // Get instagram user name
-        // $username = $oAuth->username;
-
-        // // do your code here
 
     }
 }
