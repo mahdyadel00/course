859adc3eac539819205e8a40ab7e10783139c87e
mahdygeexar <mahdy@geexar.dev>
2023-02-19 11:30:43 +0200

    login by instgram

--- app/Http/Controllers/Frontend/InstgramController.php
+++ app/Http/Controllers/Frontend/InstgramController.php
@@ -14,7 +14,7 @@ class InstgramController extends Controller
 
     public function providerInstgram()
     {
-        $appId = config('services.instagram.client_id');
+        $appId = '540013058117269';
         dd( $appId);
         $redirectUri = urlencode(config('services.instagram.redirect'));
         return redirect()->to("https://api.instagram.com/oauth/authorize?app_id={$appId}&redirect_uri={$redirectUri}&scope=user_profile,user_media&response_type=code");
