03a0d211a6c5c35f5e9b1fe46d1f7dd0bae7ca23
mahdyadel00 <mahdyadel00@gmail.com>
2023-02-23 18:08:52 +0200

    update start time

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -24,9 +24,10 @@ class HomeController extends Controller
         $speakers = Speaker::get();
         $main_sponsers = Sponser::where('type' , 'Main')->get();
         $others_sponsers = Sponser::where('type' , 'Others')->get();
-        $topic_firstDay = TopicSpeaker::with('speaker')->where('days' , 'First Day')->get();
-        $topic_secondDay = TopicSpeaker::with('speaker')->where('days' , 'Second Day')->get();
-        $topic_thirdDay = TopicSpeaker::with('speaker')->where('days' , 'Third Day')->get();
+        $topic_firstDay = TopicSpeaker::with('speaker')->where('days' , 'First Day')->orderBy('start_time' , 'asc')->get();
+        // dd($topic_firstDay);
+        $topic_secondDay = TopicSpeaker::with('speaker')->where('days' , 'Second Day')->orderBy('start_time' , 'asc')->get();
+        $topic_thirdDay = TopicSpeaker::with('speaker')->where('days' , 'Third Day')->orderBy('start_time' , 'asc')->get();
         $company_speakers = CompanySpeaker::get();
 
         return view('frontend.layouts.index', compact('sliders', 'setting' , 'gifit' , 'speakers', 'main_sponsers', 'others_sponsers' , 'topic_firstDay' , 'topic_secondDay' , 'topic_thirdDay' , 'company_speakers'));
